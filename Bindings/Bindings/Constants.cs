1763ae6ffa76d3cb0e224f00007c6f78c3db72b6
JeanNSHUTI <16186@ecam.be>
2021-06-15 13:57:52 +0200

    Add files via upload

---
+++ Bindings/Bindings/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindings
+{
+    class Constants
+    {
+        public const int MAX_PLAYERS = 2;
+        public const int PORT = 5858;
+        public const int SCORE_OVAL_WIDTH = 10;
+        public const int SCORE_OVAL_HEIGHT = 9;
+        public const int MAX_LEVELS = 7;
+        public const int WIN_POINTS = 100;
+        public const int WhiteSmokeR = 245;
+        public const string IP_ADDRESS = "127.0.0.1";
+    }
+}
---
+++ Bindings/Bindings/NetworkPackets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindings
+{
+    //Key-Packet declarations
+    //Key in packet defining format of data from server to client
+    //Client has to listen to server packets
+    public enum ServerPackets
+    {
+        SConnectionOK = 1,
+    }
+
+
+    //Get send from client to server
+    //Server has to listen to client packets
+    public enum ClientPackets
+    {
+        CAcknowledge = 1,
+    }
+}
